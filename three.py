6fb5ca0130422dced093c6493801cbaa940eda2a
Tom Womack <tom@womack.net>
2019-11-25 22:12:10 +0000

    Add an object in a DRO and a vehicle in LEO, with correct velocity vectors

--- three.py
+++ three.py
@@ -6,13 +6,14 @@ import copy
 G=6.673e-11
 AU=1.49597e11
 masses=[1.989e30, 5.972e24]
-massy=[0,0,0, 0,-(29800*masses[1]/masses[0]),0,  1.49597e11,0,0, 0,29800,0]
+massy=[0,0,0, 0,-(29800*masses[1]/masses[0]),0,  AU,0,0, 0,29800,0]
 DRO=1.2
-zeromass=[DRO*AU,0,0,0,29800/sqrt(DRO),0]
+LEO=6371000+800000 # low Earth orbit is above Earth's surface not Earth's core
+zeromass=[DRO*AU,0,0,0,24320,0,  AU,LEO,0, 7455,29800,0]
 
 universe=[masses, massy, zeromass]
 
-dt=3600
+dt=60.0
 
 def derivs(masses, massy, zeromass):
   n=len(massy)/6
@@ -56,15 +57,21 @@ def derivs(masses, massy, zeromass):
    
   return [v,vz]
 
+f=open("pingle","w")
 T = 0
-for q in range(365*24*6*(3600/dt)):
+for q in range(1000000):
  olduniverse=copy.deepcopy(universe)
  # distance Earth->Sun
  ug = sqrt((universe[1][6]-universe[1][0])**2+(universe[1][7]-universe[1][1])**2+(universe[1][8]-universe[1][2])**2)
  # two velocities
  v1 = sqrt(sum(t**2 for t in universe[1][3:5]))
  v2 = sqrt(sum(t**2 for t in universe[1][9:11]))
- print T,ug,v1,v2,universe[1][9],universe[1][10],universe[1][6],universe[1][7],universe[2][0],universe[2][1],universe[2][3],universe[2][4]
+ towrite=[T,ug,v1,v2,universe[1][9],universe[1][10],universe[1][6],universe[1][7],universe[2][0],universe[2][1],universe[2][6],universe[2][7]]
+ towrite=[T,sqrt((universe[2][6]-universe[1][6])**2+(universe[2][7]-universe[1][7])**2)]
+ for u in towrite:
+  f.write(str(u))
+  f.write(" ")
+ f.write("\n")
 
  # mid-point method: take half a step then another half-step
  midpoint=copy.deepcopy(universe)
@@ -81,8 +88,8 @@ for q in range(365*24*6*(3600/dt)):
  for i in range(len(Dz)):
   universe[2][i] = universe[2][i] + dt * Dz[i]
  
-# if (universe[1][3]>0 and olduniverse[1][3]<0):
-#  print "X velocity of planet changes sign at ",T
-# if (universe[2][3]>0 and olduniverse[2][3]<0):
-#  print "X velocity of satellite changes sign at ",T
+ if (universe[1][3]>0 and olduniverse[1][3]<0):
+  print "X velocity of planet changes sign at ",T
+ if (universe[2][3]>0 and olduniverse[2][3]<0):
+  print "X velocity of satellite changes sign at ",T
  T=T+dt
