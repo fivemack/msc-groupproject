7761ffd393a5db085704e3c23557f8013ee561e8
Tom Womack <tom@womack.net>
2020-04-19 11:48:00 +0100

    Field of view as a function of focal length

--- telescope/telescope-sensitivity.py
+++ telescope/telescope-sensitivity.py
@@ -1,6 +1,7 @@
-from math import exp,pi
+from math import exp,pi,sqrt,sin,cos,tan
 
 arcsec_per_radian = (180*3600)/pi
+degrees = pi/180 # convert degrees to radians
 
 stefan_boltzmann = 5.67e-8
 AU = 1.5e11
@@ -37,4 +38,23 @@ FOV_pixels = 4096
 focal_length = pixels_per_dl * H2RG_pixel_size / dl_radians
 focal_ratio = focal_length / telescope_diameter
 print "Focal length for %s pixels per diffraction-limit is %.2f metres, ratio %.2f" % (pixels_per_dl, focal_length, focal_ratio)
+
+FOV_width_metres = FOV_pixels * H2RG_pixel_size
+FOV_width_radians = FOV_pixels * H2RG_pixel_size / focal_length
+FOV_width_degrees = FOV_width_radians*arcsec_per_radian/3600
+
+print "Side length of field of view for %s-pixel detector array is %.3f degrees" % (FOV_pixels, FOV_width_degrees)
+print "Field of view has %.3g-metre diagonal" % (sqrt(2)*FOV_width_metres)
+
+FOV_area_squaredeg = FOV_width_degrees**2 
+sky_area_squaredeg = 360*360/pi
+
+# but we have a 45-degree exclusion window around the Sun
+sun_exclusion_degrees = 45
+
+sky_area_squaredeg = sky_area_squaredeg * (1+cos(sun_exclusion_degrees*degrees))/2
+
+fields_per_sky = sky_area_squaredeg / FOV_area_squaredeg
+
+print "%.4g fields-of-view tile the usable sky" % fields_per_sky
 
